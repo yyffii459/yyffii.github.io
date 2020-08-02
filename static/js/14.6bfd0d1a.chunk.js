751bdaaa8682f5c8037c3a1639060bfa9439d9c0
yyffii <yyffii.finance>
2020-08-02 14:39:33 +0200

    Updates

---
+++ static/js/14.6bfd0d1a.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpyyffii-finance"]=this["webpackJsonpyyffii-finance"]||[]).push([[14],{1666:function(i,n){}}]);
+//# sourceMappingURL=14.6bfd0d1a.chunk.js.map
---
+++ static/js/9.63ea6062.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpyyffii-finance"]=this["webpackJsonpyyffii-finance"]||[]).push([[9],{1758:function(i,n){},1766:function(i,n){}}]);
+//# sourceMappingURL=9.63ea6062.chunk.js.map
